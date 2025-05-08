ae5dbe91062b79034ceaf29aa67c44d9c4ea7380
Kesavan AV <avkesavan2000@gmail.com>
2025-05-08 06:29:45 +0530

    feat:new code added

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo
 {
     public static void main(String[] args)
     {
-      System.out.println(2+3);  
+      System.out.println((2+3)+(5-2);  
       System.out.println("Amineri"+" Kesavan");
     }
 }
